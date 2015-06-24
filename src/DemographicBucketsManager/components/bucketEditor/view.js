e05f1cc55ff25616b4d93a5adab7b788020716a8
Dwayne Jones <djones@permissiondata.com>
2015-06-24 18:24:37 -0400

    SER-209 Mocked UI

--- src/DemographicBucketsManager/components/bucketEditor/view.js
+++ src/DemographicBucketsManager/components/bucketEditor/view.js
@@ -1,6 +1,6 @@
 import {inject} from 'aurelia-framework';
 import {HttpClient} from 'aurelia-http-client';
-import {Services} from '../services';
+import {Services} from '../../../services';
 
 @inject(HttpClient)
 export class LeadFileGen {
@@ -10,7 +10,26 @@ export class LeadFileGen {
   		this.rowLimit = 10;
   		//this.outputTemplate = "Id, Email, Name, Location\n{{range $row := . }}{{ $row.uuid }},{{ $row.email }},{{ $row.first_name }} {{ $row.last_name }},\"{{ $row.city }}, {{ $row.state }}, {{ $row.zipcode }}\"\n{{ end }}\n";
   		this.results = "";
-  
+      this.generate();
+  }
+
+
+  generate() {
+  	var url = Services.CampaignBuckets();
+  	var that = this;
+
+  	this.http.get(url).then(function (httpResponse) {
+
+  		that.setResults(httpResponse.response);
+  		
+  	});
+
+  	
+  }
+
+  setResults(results){
+  	this.results = results;
+      console.log(results);
   }
 
 }
